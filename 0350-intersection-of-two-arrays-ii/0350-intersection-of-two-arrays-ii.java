1fd100d8dce08f4e293c31f6097a092f9162efbc
Ramandeep Singh <omraman3333@gmail.com>
2026-01-31 02:05:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,31 +1,52 @@
+// class Solution {
+//     public int[] intersect(int[] arr1, int[] arr2) {
+//                ArrayList<Integer> ans = new ArrayList<>();
+
+//         int n = arr1.length;
+//         int m = arr2.length;
+
+//         for (int i = 0; i < n; i++) {
+
+//             for (int j = 0; j < m; j++) {
+
+//                 if (arr1[i] == arr2[j]) {
+                       
+//                         ans.add(arr1[i]);
+//                         arr2[j]=Integer.MIN_VALUE;
+//                         break;
+//                 }
+//             }
+//         }
+
+// //upper dhyan se dekho wha array return karna hai hm arraylist return nhi kar sakte 
+// //similarly agar arraylist hota tab array return nhi , so is case me hm fixed  array 
+// // size bana rhe hai and use return karenge
+//         int[] result = new int[ans.size()];
+//         for (int k = 0; k < ans.size(); k++) {
+//             result[k] = ans.get(k);
+//         }
+
+//         return result;
+//     }
+// }
 class Solution {
     public int[] intersect(int[] arr1, int[] arr2) {
-               ArrayList<Integer> ans = new ArrayList<>();
-
+        ArrayList<Integer> ans = new ArrayList<>();
         int n = arr1.length;
         int m = arr2.length;
-
-        for (int i = 0; i < n; i++) {
-
-            for (int j = 0; j < m; j++) {
-
-                if (arr1[i] == arr2[j]) {
-                       
-                        ans.add(arr1[i]);
-                        arr2[j]='a';
-                        break;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(arr1[i]==arr2[j]){
+                    ans.add(arr1[i]);
+                   
+                    break;
                 }
             }
         }
-
-//ye yaad rakhna ki seedha ARR likhne se wo Array return nhi hota 
-//  yaad rakhna loop laga ke usko ek dusre Array me dale and uske i ko get kare 
-// phir usko return kare.
         int[] result = new int[ans.size()];
         for (int k = 0; k < ans.size(); k++) {
             result[k] = ans.get(k);
         }
-
         return result;
     }
 }
