4b77492e8c93243c90907d6fbb6a74e9c79a1016
Ramandeep Singh <omraman3333@gmail.com>
2026-03-11 23:37:41 +0530

    Time: 1 ms (99.94%), Space: 46.8 MB (91.51%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,68 @@
 class Solution {
-    public int singleNumber(int[] arr) {
-      int n = arr.length;
-      if(n==1) return arr[0];
-      int i=0;
-      Arrays.sort(arr);
-      for( i =1;i<n;i=i+2){
-        if(arr[i-1]==arr[i])
-         continue;
-        else if(arr[i-1]!=arr[i]) 
-        return arr[i-1];
-        //  else if(arr[n-1]!=arr[n-2])
-        // return arr[n-1];
-        
-      }  
-      return arr[i-1];      
+    public int singleNumber(int[] nums) {
+        int res=0;
+        for(int i: nums){
+            res=res^i;
+        }
+        return res;
     }
 }
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int num : nums){
+//             if(set.contains(num)){
+//                 set.remove(num);
+//             } else {
+//                 set.add(num);
+//             }
+//         }
+//         for(int num : set){
+//             return num;
+//         }
+
+//         return -1;
+//        // return set.iterator().next();
+//     }
+// }
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int num : nums){
+//             if(map.containsKey(num)){
+//                 map.put(num, map.get(num) + 1);
+//             } else {
+//                 map.put(num, 1);
+//             }
+//         }
+
+//         for(int key : map.keySet()){
+//             if(map.get(key) == 1){
+//                 return key;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
+// // class Solution {
+//     public int singleNumber(int[] arr) {
+//       int n = arr.length;
+//       if(n==1) return arr[0];
+//       int i=0;
+//       Arrays.sort(arr);
+//       for( i =1;i<n;i=i+2){
+//         if(arr[i-1]==arr[i])
+//          continue;
+//         else if(arr[i-1]!=arr[i]) 
+//         return arr[i-1];
+//          else if(arr[n-1]!=arr[n-2])
+//         return arr[n-1];
+        
+//       }  
+//       return arr[i-1];      
+//     }
+// }
