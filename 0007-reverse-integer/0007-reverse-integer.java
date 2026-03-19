d98176febaee45a6c9e2bd7b3d749a3c4f8d7876
Ramandeep Singh <omraman3333@gmail.com>
2026-03-19 11:42:28 +0530

    Time: 1 ms (99.9%), Space: 42.2 MB (97.38%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,3 +1,17 @@
+// class Solution {
+//     public int reverse(int x) {
+//         int r = 0;
+//         int copy=x;
+//        if(copy<0) copy=-copy;
+//         for(int i=0;copy>0;i++){
+//             int v = copy%10;
+//             r=r*10 + v;
+//             copy/=10;
+//         }
+//          if(x<0)  return(-r);
+//          else return(r);
+//     }
+// }
 class Solution {
     public int reverse(int n) {
         int rev=0;
@@ -5,6 +19,7 @@ class Solution {
             int dig=n%10;
                if (rev > Integer.MAX_VALUE / 10 || rev < Integer.  MIN_VALUE / 10)
                 return 0;
+                // hm n ko nhi rev ko dekh rhe hai isliye ise loop ke andar likhe hai.
             rev = rev*10+dig;
             n = n/10;
         }
