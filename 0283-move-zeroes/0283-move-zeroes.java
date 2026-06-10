988f4e5fafcada88e0cef580c36a5845186acdd3
Ramandeep Singh <omraman3333@gmail.com>
2026-06-10 13:50:58 +0530

    Time: 2 ms (91.87%), Space: 47.9 MB (37.11%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+
+        int index = 0; 
+        for(int i =0;i<arr.length;i++){
+            if (arr[i]!=0){
+                 arr[index]=arr[i];
+                 index++;
+        } 
+      }
+      while(index<arr.length){
+        arr[index]=0;
+        index++;
+      }
+    }
+}
